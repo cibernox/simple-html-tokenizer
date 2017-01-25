ddb7e9d84cf3c9bea71196b8373d3c2eeab2b0b1
Ricardo Mendes <rokusu@gmail.com>
2017-01-25 01:35:57 +0000

    bring branch closer to master

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -10,18 +10,17 @@ export default class EntityParser {
   constructor(private named: Dict<string>) {}
 
   parse(entity: string): string | undefined {
-    if (!entity) return;
-
+    if (!entity) {
+      return;
+    }
     let matches = entity.match(HEXCHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 16));
     }
-
     matches = entity.match(CHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 10));
     }
-
     matches = entity.match(NAMED);
     if (matches) {
       return this.named[matches[1]];
