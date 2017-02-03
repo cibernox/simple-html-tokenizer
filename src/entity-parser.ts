724cbceb06fa669711699fea3c56db100dbd70fa
Ricardo Mendes <rokusu@gmail.com>
2017-02-03 15:51:17 +0000

    Make entity-parser closer to master branch

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -7,21 +7,22 @@ const CHARCODE    = /^#([0-9]+)$/;
 const NAMED       = /^([A-Za-z0-9]+)$/;
 
 export default class EntityParser {
-  constructor(private named: Dict<string>) {}
+  constructor(private named: Dict<string>) {
+  }
 
   parse(entity: string): string | undefined {
-    if (!entity) return;
+    if (!entity) {
+      return;
+    }
 
-    var matches = entity.match(HEXCHARCODE);
+    let matches = entity.match(HEXCHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 16));
     }
-
     matches = entity.match(CHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 10));
     }
-
     matches = entity.match(NAMED);
     if (matches) {
       return this.named[matches[1]];
